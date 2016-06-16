f5bff2113150cca7fdba479701ad283b8816d2cb
laowucqy <648238463@qq.com>
2016-06-16 10:06:26 +0800

    准备加入数据库以及登陆处理

--- filter_factory.py
+++ filter_factory.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 
 class filter_factory(object):
     def __init__(self, app):
@@ -17,3 +18,19 @@ class filter01(filter_factory):
     def __call__(self, environ, start_response):
         print 'request method is: %s' % environ['REQUEST_METHOD']
         return self.app(environ, start_response)
+
+class filter02(filter_factory):
+    def __init__(self, app):
+        self.req_usernames = ['LAOWU']
+        super(filter02, self).__init__(app)
+
+    def __call__(self, environ, start_response):
+        pprint(environ)
+        if environ.get('REMOTE_USER') in self.req_usernames:
+            return self.app(environ, start_response)
+        start_response(
+            '403 Forbidden', [('Content-type', 'text/html')])
+        return ['You are forbidden to view this resource']
+
+
+
